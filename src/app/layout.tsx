2de99d4b04c74093ddc7e35b1e927e6ac924aea6
vic0906 <vic0906@hotmail.com>
2026-04-24 11:51:22 +0800

    feat: add Baidu auto-push JS for automatic URL discovery

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,6 +73,26 @@ export default function RootLayout({
             `,
           }}
         />
+        {/* 百度自动推送 */}
+        <Script
+          id="baidu-push"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(){
+                var bp = document.createElement('script');
+                var curProtocol = window.location.protocol.split(':')[0];
+                if (curProtocol === 'https') {
+                  bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+                } else {
+                  bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+                }
+                var s = document.getElementsByTagName("script")[0];
+                s.parentNode.insertBefore(bp, s);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   );
